bc8f0e283d059ba2bd6e3771f95f91ad71fbc817
AdrianCardonaCandil <adriancardonacandil@gmail.com>
2025-03-16 02:42:49 +0000

    delete debug comment on firebase initialization

--- backend/server/config/firebase.js
+++ backend/server/config/firebase.js
@@ -8,7 +8,6 @@ const {getAuth} = require('firebase-admin/auth');
 
 try {
     const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
-    console.log("hello")
     if (getApps().length === 0){
         initializeApp({
             credential: cert(serviceAccount)
